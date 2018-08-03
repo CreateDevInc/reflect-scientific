46e787856dee9b67d235f6683fb5997c6ea4ff2b
Jordin Gardner <gard.jordin@gmail.com>
2018-08-03 16:45:12 -0600

    added loader on releases page

--- app/src/js/releases.js
+++ app/src/js/releases.js
@@ -1,100 +1,93 @@
 if (/releases.html$/.test(window.location.href)) {
+  const apiURL =
+    "http://ec2-35-171-6-228.compute-1.amazonaws.com/announcements/index.php/wp-json/wp/v2/posts";
 
-    const apiURL = "http://ec2-35-171-6-228.compute-1.amazonaws.com/announcements/index.php/wp-json/wp/v2/posts";
+  const postContainer = document.getElementsByClassName("post-container")[0];
 
-    const postContainer = document.getElementsByClassName('post-container')[0];
-    
-    const ajax = new XMLHttpRequest();
+  const ajax = new XMLHttpRequest();
 
-    ajax.open('GET', apiURL);
-    ajax.responseType = "json";
+  ajax.open("GET", apiURL);
+  ajax.responseType = "json";
 
-    ajax.onload = () => {
-        let postArr = [];
-        for (let i = 0; i < ajax.response.length; i++) {
-            
-            
-            const aResponse = ajax.response[i];
-            
+  ajax.onload = () => {
+    let postArr = [];
+    for (let i = 0; i < ajax.response.length; i++) {
+      const aResponse = ajax.response[i];
 
-            const title = aResponse.title.rendered;
-            const dateField = new Date(aResponse.date);
+      const title = aResponse.title.rendered;
+      const dateField = new Date(aResponse.date);
 
-            const content = document.createElement('div');
-            content.classList.add('exerpt-limit');
-            content.id = 'content' + i;
-            content.innerHTML = aResponse.content.rendered;
-            
+      const content = document.createElement("div");
+      content.classList.add("exerpt-limit");
+      content.id = "content" + i;
+      content.innerHTML = aResponse.content.rendered;
 
-            //read more button and function
-            function handleClick(index) {
-                if (document.getElementById('button' + index).textContent == "Read More") {
-                    document.getElementById('content' + index).classList.remove('exerpt-limit');
-                    document.getElementById('fade' + index).classList.remove('fade-out-div');
-                    document.getElementById('button' + index).textContent = "Show Less";
-                } else {
-                    document.getElementById('content' + index).classList.add('exerpt-limit');
-                    document.getElementById('fade' + index).classList.add('fade-out-div');
-                    document.getElementById('button' + index).textContent = "Read More";
-                }
-                
-            }
+      //read more button and function
+      function handleClick(index) {
+        if (
+          document.getElementById("button" + index).textContent == "Read More"
+        ) {
+          document
+            .getElementById("content" + index)
+            .classList.remove("exerpt-limit");
+          document
+            .getElementById("fade" + index)
+            .classList.remove("fade-out-div");
+          document.getElementById("button" + index).textContent = "Show Less";
+        } else {
+          document
+            .getElementById("content" + index)
+            .classList.add("exerpt-limit");
+          document.getElementById("fade" + index).classList.add("fade-out-div");
+          document.getElementById("button" + index).textContent = "Read More";
+        }
+      }
 
-            const readMore = document.createElement('a');
-            readMore.classList.add('read-more-btn');
-            readMore.id = 'button' + i;
-            readMore.textContent = "Read More";
-            
-            
-            
-            
-            
-            const post = document.createElement('div');
-            post.classList.add('hidden-post', 'post');
-            post.innerHTML =`
+      const readMore = document.createElement("a");
+      readMore.classList.add("read-more-btn");
+      readMore.id = "button" + i;
+      readMore.textContent = "Read More";
+
+      const post = document.createElement("div");
+      post.classList.add("hidden-post", "post");
+      post.innerHTML = `
             <div class='relative-container'> 
                 <h2>${title}</h2>
-                <h3>${new Intl.DateTimeFormat('en-US').format(dateField)}</h3>
+                <h3>${new Intl.DateTimeFormat("en-US").format(dateField)}</h3>
                 <div>${content.outerHTML}</div>
                 <div class='fade-out-div' id='fade${i}'></div>
                 ${readMore.outerHTML}
             </div>
             `;
-            
-            postContainer.append(post);
-            document.getElementById('button' + i).addEventListener('click', function() {
-                handleClick(i);
-            });
-            postArr.push(post);
-            
-        }
-        const fadeEach = (arr) => {
-            if (arr.length > 0) {
-                setTimeout(() => {
-                    arr.shift().classList.remove('hidden-post');
-                    fadeEach(arr);
-                }, 700);
-            }
-        }
-        fadeEach(postArr);
-        
+
+      postContainer.append(post);
+      document
+        .getElementById("button" + i)
+        .addEventListener("click", function() {
+          handleClick(i);
+        });
+      postArr.push(post);
+
+      if (!loaderHidden) {
+        loaderHidden = true;
+        setTimeout(function() {
+          document.querySelector(".lds-ring").classList.add("hidden");
+        }, 500);
+      }
     }
 
-    ajax.send();
+    var loaderHidden = false;
 
-    
+    const fadeEach = arr => {
+      if (arr.length > 0) {
+        setTimeout(() => {
+          arr.shift().classList.remove("hidden-post");
+          fadeEach(arr);
+        }, 700);
+      }
+    };
+    fadeEach(postArr);
+  };
 
-    
-
-    
-
-
-
+  ajax.send();
 }
-
-
-
-
-
-
-
